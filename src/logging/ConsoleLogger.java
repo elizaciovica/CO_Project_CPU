a425db143309c8f6edb4f18374bbf65133e21400
Eliza <elizaciovica0208@gmail.com>
2021-04-27 11:18:41 +0300

    logging package

---
+++ src/logging/ConsoleLogger.java
@@ -0,0 +1,29 @@
+package logging;
+
+public class ConsoleLogger implements ILogger{
+    @Override
+    public void write(long l) {
+        System.out.println(l);
+
+    }
+
+    @Override
+    public void write(String s) {
+        System.out.println(s);
+
+    }
+
+    @Override
+    public void write(Object... objects) {
+        for(Object o : objects) {
+            System.out.println(o.toString() + " ");
+        }
+        System.out.println();
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
---
+++ src/logging/FileLogger.java
@@ -0,0 +1,23 @@
+package logging;
+
+public class FileLogger implements ILogger {
+    @Override
+    public void write(long l) {
+
+    }
+
+    @Override
+    public void write(String s) {
+
+    }
+
+    @Override
+    public void write(Object... objects) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
